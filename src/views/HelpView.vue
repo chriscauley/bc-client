ba6101b377af29385c29c8a7433d531da0730aa6
chriscauley <379151+chriscauley@users.noreply.github.com>
2024-08-07 15:34:45 -0400

    help page fixed

--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="mx" id="help-view">
-    <h3 class="red-text lighten-2">How to app?</h3>
-    <div class="hr" />
-    <h4 class="red-text lighten-2">Step 1: Choose Talks</h4>
+  <div class="help-view">
+    <h2 class="h2 text-center">How to app?</h2>
+    <h4 class="h4">
+      <span class="ec yes-no-may-be-so-blob" />
+      Step 1: Choose Talks
+    </h4>
     <p class="flow-text">Swipe or tap talks to sort them into the following lists.</p>
     <ul class="browser-default">
-      <li v-for="vote in vote_list" class="mb" :key="vote.value">
+      <div class="help-view__vote" v-for="vote in vote_list" :key="vote.value">
         <span :class="vote.icon" />
         {{ vote.verbose }}
-      </li>
+      </div>
     </ul>
     <div class="hr" />
-    <h4 class="red-text lighten-2">Step 2: View Schedule</h4>
+    <h4 class="h4">
+      <span class="ec party-blob" />
+      Step 2: View Schedule
+    </h4>
     <p class="flow-text">
       Your schedule will show
       <span :class="vote_list[2].icon" />
       votes at thee top of each time slot.
     </p>
     <div class="hr" />
-    <h4 class="red-text lighten-2">Step 3: Attend Talks</h4>
+    <h4 class="h4">
+      <span class="ec party-blob-2" />
+      Step 3: Attend Talks
+    </h4>
     <p class="flow-text">
       When you attend a talk, mark your attendance in the schedule to get a
       <span class="ec ec-star" />
@@ -29,20 +37,21 @@
       If you want to use this app or know more, contact @badatmetroid on discord or checkout the
       <a href="https://github.com/chriscauley/ur-conf/">github repo</a>.
     </p>
-    <div class="hr" />
-    <!-- <h4 class="red-text lighten-2">Step &infin;: Control TIME!!!</h4> -->
+    <!-- <h4 class="h4">Step &infin;: Control TIME!!!</h4> -->
     <!-- <p class="flow-text"> -->
     <!--   Click the clock to speed up time to 0, 5, or 15 minutes per second. -->
     <!--   Double click to reset. Use this power wisely, or you will break time. -->
     <!-- </p> -->
-    <div class="hr" />
-    <p class="center flow-text">What are you waiting for?</p>
-    <div class="center mb">
+    <!-- <div class="hr" /> -->
+    <div class="help-view__actions text-center">
+      <p>What are you waiting for?</p>
       <router-link to="/vote/">
         <button class="btn -primary">Start Choosing Talks</button>
       </router-link>
     </div>
-    <p class="flow-text" style="whitespace: pre-line" @click="next">{{ rand_text }}</p>
+    <div class="hr" />
+    <div class="hr" />
+    <p class="help-view__random-text" @click="next">{{ rand_text }}</p>
   </div>
 </template>
 
@@ -50,17 +59,18 @@
 import { vote_list } from '@/lib/vote'
 
 const rand_texts = [
-  'On one hand, the stars are worthless. On the other hand... shiny!',
-  'Each star is worth one point. Points are great, right?',
+  'On one hand, the stars are worthless.\nOn the other hand... shiny!',
+  'Each star is worth one point.\nPoints are great, right?',
   'Whovever has the most stars wins a prize. \nThe prize is an even bigger star.',
-  "That's all. That's all this app does. What did you expect?",
-  'Step 4: ...\nStep 5: ...\n...\n Profit?',
+  "That's all. That's all this app does.\nWhat did you expect?",
+  'Step 4: ...\nStep 5: ...\n...\n...\n...\n Profit?',
+  "There are way too many easter eggs on this site.\nI'm sorry. I get bored easily.",
 ]
 
 export default {
   data() {
     return {
-      vote_list,
+      vote_list: vote_list.toReversed(),
       rand_index: Math.floor(Math.random() * rand_texts.length),
     }
   },
--- src/views/SessionDetailView.vue
+++ src/views/SessionDetailView.vue
@@ -9,8 +9,8 @@ export default {
   computed: {
     session() {
       const id = Number(this.$route.params.session_id)
-      return this.$store.app.get().sessions?.find(s => s.id === id)
-    }
-  }
+      return this.$store.app.get().sessions?.find((s) => s.id === id)
+    },
+  },
 }
 </script>
